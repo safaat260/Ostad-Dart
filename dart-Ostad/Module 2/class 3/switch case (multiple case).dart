5af67056e8ed6901a96b280aabbe12cdd3c7a231
Fahim <fahimsafaat67@gmail.com>
2025-03-17 03:09:13 +0600

    switch case (multiple case).dart

--- dart-Ostad/Module 2/class 3/switch case (multiple case).dart
+++ dart-Ostad/Module 2/class 3/switch case (multiple case).dart
@@ -1,8 +1,35 @@
 import 'dart:io';
 //multiple case
 main() {
-  print("Enter month: ");
-  String ? month = stdin.readLineSync();
+  print("Enter month number: ");
+  int ? month = int.tryParse(stdin.readLineSync()!);
 
+  switch(month){
+    //multiple with single single case
+    case 12:
+    case 1:
+    case 2:
+      print("It's Winter!");
+        break;
+
+    case 3:
+    case 4:
+    case 5:
+    print("It's Spring!");
+      break;
+
+
+    //multiple with OR operator
+    case 6 || 7 || 8:
+      print("It's Summer!");
+      break;
+
+    case 9 || 10 || 11:
+      print("It's Autumn!");
+      break;
+
+    default:
+      print("Invaild Month!");
+  }
 
 }
