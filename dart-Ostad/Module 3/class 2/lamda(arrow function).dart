78f7b2605d6fca8e39f317e7a21dd51cd1725f99
Fahim <fahimsafaat67@gmail.com>
2025-03-23 17:43:29 +0600

    practice

--- dart-Ostad/Module 3/class 2/lamda(arrow function).dart
+++ dart-Ostad/Module 3/class 2/lamda(arrow function).dart
@@ -3,6 +3,10 @@ import 'dart:math';
 ////toStringAsFixed(4) is for showing 4 digit after .
 var circle = ({required double pi, required double radius}) => (pi*(pow(radius, 2))).toStringAsFixed(4);
 
+var area = (int length, int width) => (length*width);
+
 main(){
   print("area of circle is: ${circle(pi:3.1416, radius: 5)}");
+
+  print("area is: ${area(5,6)}");
 }
