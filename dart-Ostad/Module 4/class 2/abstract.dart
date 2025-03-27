3b8944810e4a5dfabf18ab2b0b88c66216860914
Fahim <fahimsafaat67@gmail.com>
2025-03-28 03:30:54 +0600

    abstract.dart

--- dart-Ostad/Module 4/class 2/abstract.dart
+++ dart-Ostad/Module 4/class 2/abstract.dart
@@ -1,12 +1,21 @@
-/*we use abstract when we dont want to give permission create an
+/*1.we use abstract when we dont want to give permission create an
 object by the name of main class
-if we want to use it we need to extend it from parent class then we can use the class properties*/
+2.if we want to use it we need to extend it from parent class then we can use the class properties
+3.if we use abstract we must >>override/just declare<< the all the methods!!
+*/
 
 abstract class Animal{
   int eye = 2;
+  fly();//body less abstract methods
 }
 class Eagle extends Animal{//just extend no parameter is acceptable
+
+  fly(){//methods must be declare/override
+    print("Eagle flys");
+  }
+
 }
+
 main(){
 
   /*Animal ani = Animal();//not possible for abstract need to extend
@@ -14,5 +23,6 @@ main(){
 
   Eagle bird = Eagle();
   print(bird.eye);
+  bird.fly();
 
 }
