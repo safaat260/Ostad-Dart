b0c3aeb3a824a91cd170e1e6673d55eaa98992cd
Fahim <fahimsafaat67@gmail.com>
2025-04-08 22:13:24 +0600

    list map

---
+++ dart-Ostad/Module 2/class 1/map inside list.dart
@@ -0,0 +1,9 @@
+void main() {
+  List<Map<String, dynamic>> students = [
+    {"name": "Alice", "scores": [85, 90, 78]},
+    {"name": "Bob", "scores": [88, 76, 95]},
+    {"name": "Charlie", "scores": [90, 92, 85]},
+  ];
+
+ print(students);
+}
