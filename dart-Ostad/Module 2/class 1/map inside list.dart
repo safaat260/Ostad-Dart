ae45e36ac579ca470a28d346411e99cc266e00bc
Fahim <fahimsafaat67@gmail.com>
2025-04-09 20:30:18 +0600

    update

--- dart-Ostad/Module 2/class 1/map inside list.dart
+++ dart-Ostad/Module 2/class 1/map inside list.dart
@@ -1,6 +1,6 @@
 //map inside list
 main() {
-  List<Map<String, dynamic>> students = [      // map inside list
+  List<Map<String, dynamic>> students = [     // map inside list
     {"name": "Alice", "scores": [85, 90, 78]},
     {"name": "Bob", "scores": [88, 76, 95]},
     {"name": "Charlie", "scores": [90, 92, 85]},
