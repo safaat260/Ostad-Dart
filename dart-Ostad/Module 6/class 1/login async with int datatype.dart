9804a9378692a38d9f4b8b3713b6add759d76a47
Fahim <fahimsafaat67@gmail.com>
2025-04-17 22:31:36 +0600

    async

---
+++ dart-Ostad/Module 6/class 1/login async with int datatype.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+Future<int> login(int num, String pass) async{
+  if(num == 0174549 && pass == "abc12"){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+
+}
+
+main(){
+  print("Enter user number: ");
+  int number = int.parse(stdin.readLineSync()!);
+
+  print("Enter user pass: ");
+  String ? password = stdin.readLineSync()!;
+
+  login(number, password).then((value)
+  {
+    if(value == 1){
+      print("facebook homepage!");
+    }
+
+    else{
+      print("Try again!");
+    }
+
+  }
+);
+
+}
