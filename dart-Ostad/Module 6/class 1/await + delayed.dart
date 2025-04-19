9e5c9bd4b2d139fd46dce9b54257341263063e63
Fahim <fahimsafaat67@gmail.com>
2025-04-19 22:24:43 +0600

    async await delayed

--- dart-Ostad/Module 6/class 1/await + delayed.dart
+++ dart-Ostad/Module 6/class 1/await + delayed.dart
@@ -2,6 +2,7 @@
 1)delayed means waiting in seconds of a function/work .(dot)delayed(Duration(seconds: 5),()){......};
 2)await makes the whole function run with delayed then execute the next part/work.
  */
+
 Future<void> test() async{
  await Future.delayed(Duration(seconds: 3), (){//delayed and Duration
     print("Loading...Loading....");
@@ -9,11 +10,10 @@ Future<void> test() async{
 );
   print("3 Seconds waiting Done!");
 }
-
+//check
 main() async {
   print("Program Start");
   print("Wait 3 Seconds!");
-
  await test();//for await async must be done in main() function
   print("Program Done!");
 }
